69937ae1e4f2f1fa6e5d533c5ec3461ad26717cb
sikieiki <emilia.schweinzer@gmail.com>
2025-05-11 18:26:27 +0200

    Expand metatiles from 1025 to 4096

--- include/fieldmap.h
+++ include/fieldmap.h
@@ -3,13 +3,16 @@
 
 #define NUM_TILES_IN_PRIMARY 640
 #define NUM_TILES_TOTAL 1024
-#define NUM_METATILES_IN_PRIMARY 640
-#define NUM_METATILES_TOTAL 1024
+#define NUM_METATILES_IN_PRIMARY 2560
+#define NUM_METATILES_TOTAL 4096
 #define NUM_PALS_IN_PRIMARY 7
 #define NUM_PALS_TOTAL 13
 #define MAX_MAP_DATA_SIZE 10240
 
+#define MAX_ELEVATION_LEVEL 7 // Old Was 15
+
 #define NUM_TILES_PER_METATILE 12
+
 
 // Map coordinates are offset by 7 when using the map
 // buffer because it needs to load sufficient border
