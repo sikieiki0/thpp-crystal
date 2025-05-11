1f260e80bac985f17ab5793bd13c489ce39f3c86
sikieiki <emilia.schweinzer@gmail.com>
2025-05-11 22:41:10 +0200

    Insert Johto/Kanto maps

--- src/new_game.c
+++ src/new_game.c
@@ -130,7 +130,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(WAKABA_TOWN), MAP_NUM(WAKABA_TOWN), WARP_ID_NONE, 20, 6);
     WarpIntoMap();
 }
 
