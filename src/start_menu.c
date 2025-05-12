ac25079e5b9c19b5c929b8f10e3394e02635f684
sikieiki <emilia.schweinzer@gmail.com>
2025-05-12 21:24:56 +0200

    Decap and touhouize Menu and Party screen

--- src/start_menu.c
+++ src/start_menu.c
@@ -186,7 +186,7 @@ static const struct WindowTemplate sWindowTemplate_PyramidPeak = {
     .baseBlock = 0x8
 };
 
-static const u8 sText_MenuDebug[] = _("DEBUG");
+static const u8 sText_MenuDebug[] = _("Debug");
 
 static const struct MenuAction sStartMenuItems[] =
 {
