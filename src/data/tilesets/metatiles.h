d898bfc98c61d1be0b513f4d815fc08a3c826ceb
sikieiki <emilia.schweinzer@gmail.com>
2026-04-02 18:44:23 +0200

    secondary tileset edits for johto

--- src/data/tilesets/metatiles.h
+++ src/data/tilesets/metatiles.h
@@ -407,4 +407,7 @@ const u16 gMetatileAttributes_PokemonLeague[] = INCBIN_U16("data/tilesets/second
 const u16 gMetatiles_HallOfFame[] = INCBIN_U16("data/tilesets/secondary/hall_of_fame_frlg/metatiles.bin");
 const u16 gMetatileAttributes_HallOfFame[] = INCBIN_U16("data/tilesets/secondary/hall_of_fame_frlg/metatile_attributes.bin");
 
+const u16 gMetatiles_VioletCity[] = INCBIN_U16("data/tilesets/secondary/violet_city_frlg/metatiles.bin");
+const u16 gMetatileAttributes_VioletCity[] = INCBIN_U16("data/tilesets/secondary/violet_city_frlg/metatile_attributes.bin");
+
 #endif // IS_FRLG
